c79a9ebaec3f2604931c169015e381fde6f7787c
lunoknsg <luca@noknsg.it>
2026-05-05 18:01:04 +0200

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.use(express.json({ limit: '10mb' }));
 
 let db;
